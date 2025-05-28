52f77bb637b46fb7336cca1dea8a41fb2b3ea117
Fidha Fathima M <fidhumusthafa3549@gmail.com>
2025-05-28 15:03:51 +0530

    error in production

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Home.css'
-import Navbar from '../../components/Navbar/Navbar'
+import Navbar from '../../components/Navbar/Navbar.jsx'
 import heroBanner from '../../assets/hero_banner.jpg'
 import heroTitle from '../../assets/hero_title.png'
 import playIcon from '../../assets/play_icon.png'
 import infoIcon from '../../assets/info_icon.png'
 import TitleCards from '../../components/TitleCards/TitleCards'
-import Footer from '../../components/Footer/Footer'
+import Footer from '../../components/Footer/Footer.jsx'
 
 const Home = () => {
   return (
