1fe2408e0be2405350d7ab2e8cfd45c5210bcf9c
Fidha Fathima M <fidhumusthafa3549@gmail.com>
2025-06-02 18:53:32 +0530

    path error in production

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,8 +5,8 @@ import heroBanner from '../../assets/hero_banner.jpg'
 import heroTitle from '../../assets/hero_title.png'
 import playIcon from '../../assets/play_icon.png'
 import infoIcon from '../../assets/info_icon.png'
-import TitleCards from '../../components/TitleCards/TitleCards.jsx'
-import Footer from '../../components/Footer/Footer.jsx'
+import TitleCards from '@/components/TitleCards/TitleCards.jsx'
+import Footer from '@/components/Footer/Footer.jsx'
 
 const Home = () => {
   return (
